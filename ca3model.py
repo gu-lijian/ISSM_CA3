2a11628c4c0a39d2a418249c47f50a8d889b04b3
gu-lijian <34952166+gu-lijian@users.noreply.github.com>
2019-11-19 10:11:12 +0800

    Add files via upload

--- ca3model.py
+++ ca3model.py
@@ -39,11 +39,10 @@ Path_to_clearned_file = 'D:\GoogleDrive\MTech\Courses\ISY5002 Pattern Recognitio
 # drive.mount('/content/data')
 
 def get_all_train_subject_ids():
-    train_subjects_as_ints = [3509524, 5132496, 1066528, 5498603, 2638030, 2598705, 5383425, 1455390, 4018081, 9961348, 1449548, 8258170, 781756, 9106476, 8686948, 8530312, 3997827, 4314139, 1818471, 4426783]
-    '''subjects_as_ints = [3509524, 5132496, 1066528, 5498603, 2638030, 2598705, 5383425, 1455390, 4018081, 9961348,
+    # train_subjects_as_ints = [3509524, 5132496, 1066528, 5498603, 2638030, 2598705, 5383425, 1455390, 4018081, 9961348, 1449548, 8258170, 781756, 9106476, 8686948, 8530312, 3997827, 4314139, 1818471, 4426783]
+    train_subjects_as_ints = [3509524, 5132496, 1066528, 5498603, 2638030, 2598705, 5383425, 1455390, 4018081, 9961348,
                         1449548, 8258170, 781756, 9106476, 8686948, 8530312, 3997827, 4314139, 1818471, 4426783,
-                        8173033, 7749105, 5797046, 759667, 8000685, 6220552, 844359, 9618981, 1360686, 46343,
-                        8692923] '''
+                        8173033, 7749105]
     train_subjects_as_strings = []
 
     for subject in train_subjects_as_ints:
@@ -51,11 +50,9 @@ def get_all_train_subject_ids():
     return train_subjects_as_strings
 
 def get_all_test_subject_ids():
-    test_subjects_as_ints = [6220552, 844359, 9618981, 1360686, 46343, 8692923]
-    '''subjects_as_ints = [3509524, 5132496, 1066528, 5498603, 2638030, 2598705, 5383425, 1455390, 4018081, 9961348,
-                        1449548, 8258170, 781756, 9106476, 8686948, 8530312, 3997827, 4314139, 1818471, 4426783,
-                        8173033, 7749105, 5797046, 759667, 8000685, 6220552, 844359, 9618981, 1360686, 46343,
-                        8692923] '''
+    # test_subjects_as_ints = [6220552, 844359, 9618981, 1360686, 46343, 8692923]
+    test_subjects_as_ints = [7749105, 5797046, 759667, 8000685, 6220552, 844359, 9618981, 1360686, 46343,
+                        8692923]
     test_subjects_as_strings = []
 
     for subject in test_subjects_as_ints:
@@ -184,8 +181,8 @@ def load_normalize_signal(subject_id):
     signal_motion_y = motion[2].values
     signal_motion_z = motion[3].values
     label = load_label(subject_id)
-    if len(signal_hr) > len(label):
-        hr_multiple = len(signal_hr) / len(label)
+    # if len(signal_hr) > len(label):
+    hr_multiple = len(signal_hr) / len(label)
     # if len(signal_counts) > len(label):
     #     counts_multiple = len(signal_counts) / len(label)
     # else:
@@ -203,6 +200,13 @@ def load_normalize_signal(subject_id):
         normalized_signal.append(signals_arr);
 
     return normalized_signal
+
+def randomize(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    print(permutation)
+    shuffled_dataset = dataset[permutation, :, :]
+    shuffled_labels = labels[permutation]
+    return shuffled_dataset, shuffled_labels
 
 def run_modeling(train_subject_set, test_subject_set):
     start_time = time.time()
@@ -224,14 +228,17 @@ def run_modeling(train_subject_set, test_subject_set):
         else:
             test_signal = np.concatenate((test_signal, load_normalize_signal(str(test_subject))), axis=0)
 
+    sleeping_signals_train, sleeping_labels_train = randomize(train_signal, np.array(train_label))
+    sleeping_signals_test, sleeping_labels_test = randomize(test_signal, np.array(test_label))
+
     # Extract features for both train and test signals
     start = time.time()
 
-    waveletname = 'db4'
+    waveletname = 'db38'
     print('Generate features for training data')
-    X_train, Y_train = get_sleeping_features(train_signal, np.array(train_label), waveletname)
+    X_train, Y_train = get_sleeping_features(sleeping_signals_train, np.array(sleeping_labels_train), waveletname)
     print('Generate features for testing data')
-    X_test, Y_test = get_sleeping_features(test_signal, np.array(test_label), waveletname)
+    X_test, Y_test = get_sleeping_features(sleeping_signals_test, np.array(sleeping_labels_test), waveletname)
 
     end = time.time()
     print('Time needed: %.2f seconds' % (end - start))
@@ -246,7 +253,7 @@ def run_modeling(train_subject_set, test_subject_set):
     test_score = clf.score(X_test, Y_test)
 
     print('Classification accuracy for test data set: %.4f' % test_score)
-    stage_label = ['INVALID', 'WAKE', 'N1', 'N2', 'N3', 'N4', 'REM']
+    stage_label = ['NA', 'WAKE', 'N1', 'N2', 'N3', 'N4', 'REM']
 
     Y_predict = clf.predict(X_test)
     print(pd.DataFrame(confusion_matrix(Y_test, Y_predict), index=stage_label, columns=stage_label))
